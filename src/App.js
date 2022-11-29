5ba54cf74100659afc6dc4dc59b8ad9bcf0006bd
Nasim <nasimreza72@yahoo.com>
2022-11-29 16:20:38 +0100

    added input date

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
-import { useState } from "react";
+import { useState, useRef } from "react";
 import "./App.css";
 
 function App() {
   const [urlList, setUrlList] = useState("");
+  const [helperVeribale, setHelperVeriable] = useState(false);
   const [url, setUrl] = useState("");
 
+  const dateRef = useRef();
+
   function callDatabase() {
+
+  setHelperVeriable(true)
+
    // let url = "http://localhost:3001/api/getUrl/";
    let url = "https://staging31.henrikk.sg-host.com/two/en/wp-json/wc/v3/orders?consumer_key=ck_b51b515df3f1dad1e258662058d23e36d856f673&consumer_secret=cs_17ca613d0bb25fcd36852fb305a4e14925c2be08";
 
@@ -13,14 +19,15 @@ function App() {
       .then((response) => response.json())
       .then((result) => {
 
+        console.log(dateRef.current.value)
+
         ////////////// Filtered by Date
 
-
-        let startDate = new Date("2022-11-01");
-        let endDate = new Date("2022-11-30");
+        let startDate = new Date(dateRef.current.value); //Date formet (year-month-date)
+        let endDate = new Date(); // will get automatically today's date or else type date manually
        
         let resultProductData = result.filter( order => {
-          let date = new Date( order.date_created);
+          let date = new Date( order.date_created );
           return (date >= startDate && date <= endDate);
         });
       
@@ -60,20 +67,29 @@ function App() {
       <header className="App-header">
       <img src="https://lcmed.de/wp-content/uploads/2022/10/logo-300x100-1.png" alt="Logo" />
         <h3 className="erp-dashboard"> ERP Dashboard</h3>
-        <button onClick={() => callDatabase()}>Get order data</button>
-        <div className="orderTableParent"> {urlList ? urlList.map((item) =>
 
-           <div className="orderDataTable"> 
+        <div className="label">From</div>
+        <input type="date" ref={dateRef}></input>
+
+        <button onClick={() => callDatabase()}>Get order data</button>
+
+        <div className="orderTableParent">
+
+        { helperVeribale ? 
+        
+          ( urlList ? urlList.map((item) =>
+
+            <div className="orderDataTable" key={item.id}> 
 
               <div> <span> Order id - </span>  { "#" + item.id } </div>
               <div> <span> Status - </span>{ item.status }</div>
               <div> <span> Total - </span>{ item.total }</div>
               <div> <span> Order Created - </span>{ item.date_created.replace("T", " Time ") }</div>
               <div> <span> Order Shipped - </span>{ item.date_completed? item.date_completed.replace("T", " Time "): item.shipping_status}</div>
-  
-           </div>
+              
+            </div> ) : "Loading ........")  
 
-           ) : ""} 
+         : "" }
 
         </div>
       </header>
